cc9bba0936019b5a966b3da21600de64adcec115
StoneyGravy963 <al210356@edu.uaa.mx>
2026-03-13 19:33:27 -0600

    Mejor no

--- js/bruteforce.js
+++ js/bruteforce.js
@@ -7,45 +7,13 @@
 // Cuantas mas coincidan con el texto descifrado, mas probable es que sea el correcto.
 // [26]
 const PALABRAS_ES = [
-  "el","la","los","las","lo","un","una","unos","unas",
-  "yo","tu","tú","el","él","ella","nos","nosotros","nosotras",
-  "ellos","ellas","me","te","se","le","les","mi","mis","tu","tus","su","sus",
-  "de","del","al","a","en","con","sin","por","para","sobre","entre",
-  "hasta","desde","hacia","contra","durante","mediante","segun","según","tras",
-  "y","e","o","u","ni","que","como","si","sí","pero","mas","más",
-  "aunque","porque","pues","entonces","tambien","también","ademas","además",
-  "mientras","donde","cuando","quien","quién","cual","cuál","cuales","cuáles",
-  "esto","eso","esta","este","estos","estas","ese","esa","esos","esas",
-  "aqui","aquí","ahi","ahí","alla","allá","aca","acá",
-
-  "es","son","era","eran","fue","fueron","ser","sido","sea","sean",
-  "estar","estoy","estas","estás","esta","está","estan","están","estaba","estaban",
-  "hay","haber","ha","han","hace","hacer","hacen","hizo","hice",
-  "tiene","tienen","tener","tuvo","tuvieron",
-  "puede","pueden","poder","debe","deben",
-  "dice","decir","dijo","dicen",
-  "ver","vio","visto","venir","viene","vino",
-  "ir","va","vamos","fui","fueron",
-
-
-  "hola","gracias","buenas","bueno","buena","dias","días","tardes","noches","favor",
-  "mensaje","texto","palabra","palabras","frase",
-  "codigo","código","cifrado","descifrado","seguridad","sistema","usuario",
-  "clave","llave","archivo","datos","informacion","información",
-  "resultado","resultados","analisis","análisis","prueba","intento","intentos",
-  "valor","valores","numero","número","numeros","números",
-
-
-  "muy","bien","mal","menos","todo","toda","todos","todas","nada","algo","cada","casi",
-  "solo","sólo","siempre","nunca","ya","aun","aún","antes","despues","después",
-  "primero","ultimo","último","mismo","misma","mismos","mismas","otro","otra","otros","otras",
-  "tiempo","mundo","persona","personas","casa","dia","día","semana","mes","ano","año",
-  "hoy","ayer","manana","mañana","noche","tarde","momento","forma","parte","caso","tema",
-  "nivel","proyecto","clase","ejemplo"
+  "el","la","los","las","de","del","en","que","y","a","un","una",
+  "es","se","no","con","por","su","al","lo","como","pero","mas",
+  "fue","si","sobre","este","esta","ya","entre","cuando","todo",
+  "ser","son","dos","era","hasta","desde","nos","para","le","me",
+  "mi","te","tu","hay","tiene","han","sido","muy","bien","puede",
+  "sin","tiempo","mundo","hola","gracias","buenas","aqui","esto"
 ];
-
-// Estructura de consulta rapida para mantener el analisis eficiente.
-const PALABRAS_ES_SET = new Set(PALABRAS_ES);
 
 /**
  * Puntua un texto segun cuantas palabras frecuentes del español contiene.
@@ -57,7 +25,7 @@ const PALABRAS_ES_SET = new Set(PALABRAS_ES);
 // [27]
 function scoreText(text) {
   const palabras = text.toLowerCase().match(/[a-záéíóúüñ]+/g) || [];
-  return palabras.filter((p) => PALABRAS_ES_SET.has(p)).length;
+  return palabras.filter(p => PALABRAS_ES.includes(p)).length;
 }
 
 /**
